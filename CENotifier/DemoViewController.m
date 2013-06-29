8cbc99c38ee1413a93eff7c3873d781ab45d3386
Chad Etzel <chad@jazzychad.net>
2013-06-29 16:17:32 -0700

    clean up demo files

--- CENotifier/DemoViewController.m
+++ CENotifier/DemoViewController.m
@@ -19,7 +19,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+    // Do any additional setup after loading the view.
 
     UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
     [button setTitle:@"Notify!" forState:UIControlStateNormal];
@@ -33,21 +33,23 @@
 {
     NSLog(@"view bounds: %@", NSStringFromCGRect(self.view.bounds));
     NSLog(@"button tapped!");
-    //[CENotifier displayInView:self.view image:nil title:@"Testing" text:@"This is a test" delay:3.0 userInfo:nil delegate:self];
 
     _notifyCount++;
     NSUInteger duration = arc4random_uniform(10) + 1;
 
     NSMutableString *randomText = [[NSMutableString alloc] init];
     NSUInteger ulimit = arc4random_uniform(30);
+
     for (int i = 0; i < ulimit; i++) {
         [randomText appendString:@"blah "];
     }
 
     NSString *title = [NSString stringWithFormat:@"Notification #%d", _notifyCount];
     NSString *text = [NSString stringWithFormat:@"Showing for %d seconds.\n%@", duration, randomText];
-    NSDictionary *userInfo = @{@"num": @(_notifyCount)};
-    
+    NSDictionary *userInfo = @{
+                               @"num": @(_notifyCount)
+                               };
+
     [CENotifier displayInView:self.view imageurl:@"https://si0.twimg.com/profile_images/28332812/addisontodd_bigger.jpg" title:title text:text duration:duration userInfo:userInfo delegate:self];
 }
 

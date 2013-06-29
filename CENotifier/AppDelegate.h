fe60631b190e439dce68f8501aa3d723997a26a3
Chad Etzel <chad@jazzychad.net>
2013-06-29 12:40:59 -0700

    Initial Commit

---
+++ CENotifier/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CENotifier
+//
+//  Created by Charles Etzel on 6/29/13.
+//  Copyright (c) 2013 Chad Etzel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CENotifier/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CENotifier
+//
+//  Created by Charles Etzel on 6/29/13.
+//  Copyright (c) 2013 Chad Etzel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
